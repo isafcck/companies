69736a45258848de013ff97568e08f933ebf6ee2
xiter22222 <pawanpediredla@gmail.com>
2024-08-13 18:17:15 +0530

    feat: add useTimeOut hook for delayed callback execution

--- react-coding-questions/16.ts
+++ react-coding-questions/16.ts
@@ -1,7 +1,6 @@
 
-a
-//Implement useUpdateEffect() that it works the same as useEffect() except that it skips running the callback on first render.
 
+//Implement useUpdateEffect() that it works the same as useEffect() except that it skips running the callback on first render.
 
 import React,{EffectCallBack,DependencyList} from 'react';
 //imported react and the following
